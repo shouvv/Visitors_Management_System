0b47d1b49a0a177382afef93ad995eac4f5a4692
shouvv <red10vishal@gmail.com>
2024-07-16 15:54:37 +0530

    done

--- js/script.js
+++ js/script.js
@@ -17,13 +17,3 @@ document.addEventListener('DOMContentLoaded', function() {
         document.body.classList.remove('fade-out');
     });
 });
-function updateDateTime() {
-    const now = new Date();
-    const date = now.toLocaleDateString();
-    const time = now.toLocaleTimeString();
-    document.getElementById('date').textContent = date;
-    document.getElementById('time').textContent = time;
-}
-
-setInterval(updateDateTime, 1000);
-updateDateTime();
